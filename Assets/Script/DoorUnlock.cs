b67a234e3e9ee93e3ed8fd54893dadfba9918d36
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2018-12-06 14:41:59 +0100

    La till en skylt så att man vet vart man ska gå

--- Assets/Script/DoorUnlock.cs
+++ Assets/Script/DoorUnlock.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class DoorUnlock : MonoBehaviour
 {
-    public GameObject myDoor;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            //När Player kolliderar med objekt med detta script förstörs objecktet och letar upp alla objekt med LockedDoor och tar bort dem också.
+            GameObject[] myDoor = GameObject.FindGameObjectsWithTag("LockedDoor");
+            foreach (GameObject item in myDoor)
+            {
+                Destroy(item);
+            }
             Destroy(gameObject);
         }
     }
 }
+
